21391c1844a3d1107a8b1be3711aff3f70fd4332
Sun Ning <nsun@avos.com>
2012-10-26 23:16:19 +0800

    hack compiler to create var when missing. but still got error: can't take value of a macro

--- project.clj
+++ project.clj
@@ -3,4 +3,5 @@
   :url "http://github.com/sunng87/shake"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]])
+  :dependencies [[org.clojure/clojure "1.4.0"]]
+  :java-source-paths ["java"])
--- src/shake/core.clj
+++ src/shake/core.clj
@@ -1,15 +1,10 @@
 (ns shake.core
-  (:refer-clojure :only [reify assoc
-                         symbol subs str slurp filter cond
-                         conj gensym -> defmacro print not nil? =
-                         list list? extend fn defn- eval let
-                         dorun map memfn or re-pattern])
   (:use [clojure.java.io :only [IOFactory default-streams-impl]])
   (:import [java.io File FileFilter]))
 
 (def ^:dynamic *print-output* false)
 
-(def ^:private xfilter
+#_(def ^:private xfilter
   (reify FileFilter
     (accept [this f]
       (.canExecute f))))
@@ -21,29 +16,33 @@
     :make-output-stream (fn [^Process x opts] (.getOutputStream x))))
 
 (defn- create-shake-exec-var [n]
-  (eval `(defmacro ~(symbol n) [& args#]
-           (let [str-args#
-                 (filter #(not (nil? %))
-                         (map #(cond
-                                (= (str %) "$") nil
-                                (list? %) %
-                                (.startsWith (str %) "$") (symbol (subs (str %) 1))
-                                :else (str %))
-                              args#))
-                 proc-builder-args# (conj str-args# ~n)
-                 proc-sym# (gensym "proc")]
-             `(let [~proc-sym# (.start (ProcessBuilder.
-                                        (list ~@proc-builder-args#)))]
-                (if *print-output*
-                  (print (slurp (.getInputStream ~proc-sym#)))
-                  ~proc-sym#))))))
+  (binding [*ns* (the-ns (symbol "shake.core"))]
+    (eval `(defmacro ~(symbol n) [& args#]
+             (let [str-args#
+                   (filter #(not (nil? %))
+                           (map #(cond
+                                  (= (str %) "$") nil
+                                  (list? %) %
+                                  (.startsWith (str %) "$") (symbol (subs (str %) 1))
+                                  :else (str %))
+                                args#))
+                   proc-builder-args# (conj str-args# ~n)
+                  proc-sym# (gensym "proc")]
+               `(let [~proc-sym# (.start (ProcessBuilder.
+                                          (list ~@proc-builder-args#)))]
+                  (if *print-output*
+                    (print (slurp (.getInputStream ~proc-sym#)))
+                    ~proc-sym#)))))))
 
-(defn- generate-vars [dir]
+#_(defn- generate-vars [dir]
   (let [files (map (memfn getName)
                    (-> (File. dir) (.listFiles xfilter)))]
     (dorun (map create-shake-exec-var files))))
 
-(dorun (map generate-vars
+(defn -var-missing [sym]
+  (create-shake-exec-var sym))
+
+#_(dorun (map generate-vars
             (clojure.string/split
              (or
               (System/getenv "SHAKE_PATH")
