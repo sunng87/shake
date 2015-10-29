0d29897752a46cb49240281267cda071d193a233
Fyerfox <fyerfox12@gmail.com>
2015-10-29 14:49:45 -0500

    test
    test

--- java/clojure/lang/Compiler.java
+++ java/clojure/lang/Compiler.java
@@ -6,6 +6,8 @@
  *   By using this software in any fashion, you are agreeing to be bound by
  * 	 the terms of this license.
  *   You must not remove this notice, or any other, from this software.
+ * 
+ * Free T-Shirt
  **/
 
 /* rich Aug 21, 2007 */
